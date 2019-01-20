766b674a2e522066bbdb233597fdba414c2123df
James Rowe <jroweboy@gmail.com>
2019-01-19 21:03:26 -0700

    QT: Upgrade the Loading Bar to look much better

--- src/yuzu/loading_screen.h
+++ src/yuzu/loading_screen.h
@@ -4,6 +4,7 @@
 
 #pragma once
 
+#include <chrono>
 #include <memory>
 #include <QWidget>
 
@@ -17,6 +18,10 @@ class AppLoader;
 
 namespace Ui {
 class LoadingScreen;
+}
+
+namespace VideoCore {
+enum class LoadCallbackStage;
 }
 
 class QBuffer;
@@ -43,7 +48,11 @@ public:
     // See https://wiki.qt.io/How_to_Change_the_Background_Color_of_QWidget
     void paintEvent(QPaintEvent* event) override;
 
-    void OnLoadProgress(std::size_t value, std::size_t total);
+public slots:
+    void OnLoadProgress(VideoCore::LoadCallbackStage stage, std::size_t value, std::size_t total);
+
+signals:
+    void LoadProgress(VideoCore::LoadCallbackStage stage, std::size_t value, std::size_t total);
 
 private:
 #ifndef YUZU_QT_MOVIE_MISSING
@@ -53,4 +62,14 @@ private:
 #endif
     std::unique_ptr<Ui::LoadingScreen> ui;
     std::size_t previous_total = 0;
+    VideoCore::LoadCallbackStage previous_stage;
+    std::chrono::steady_clock::time_point previous_time;
+    // newly generated shaders are added to the end of the file, so when loading and compiling
+    // shaders, it will start quickly but end slow if new shaders were added since previous launch.
+    // These variables are used to detect the change in speed so we can generate an ETA
+    bool slow_shader_compile_start = false;
+    std::chrono::steady_clock::time_point slow_shader_start;
+    std::size_t slow_shader_first_value = 0;
 };
+
+Q_DECLARE_METATYPE(VideoCore::LoadCallbackStage);
